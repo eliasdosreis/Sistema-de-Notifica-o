ec25e2cb2b681da219aa5ec067892753c04cafd7
Elias <elias_dos_reis@hotmail.com>
2020-01-29 13:07:14 -0300

    Css Finalizado

--- index.php
+++ index.php
@@ -5,8 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Sistema de Notificação</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./_cdn/fonticon.css">
+    <link rel="stylesheet" href="./_cdn/style.css">
+    
 </head>
 <body>
-    
+
+    <div class="trigger_notify_box">
+        <div class="trigger_notify trigger_notify_blue">
+            <p>Teste</p>
+            <span class="trigger_notify_timer"></span>
+        </div>
+    </div>
+
+    <h1>Aplicação de Trigger Notify</h1>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </body>
 </html>
